fcd617851997879f63bc9ae0924c895a0ab7563f
segfault <segfault@c10dc6e2-ae21-0410-890e-c4c894b30763>
2006-11-13 20:31:20 +0000

    switching to a thread to handle delivery
    git-svn-id: svn://hasno.info/mephisto/plugins/mephisto_comment_email@9 c10dc6e2-ae21-0410-890e-c4c894b30763

--- lib/comment_email_observer.rb
+++ lib/comment_email_observer.rb
@@ -1,9 +1,15 @@
 require 'comment_notifier'
+require 'thread'
 
 class CommentEmailObserver < ActiveRecord::Observer 
   observe Comment
 
   def after_save( comment )
-    CommentNotifier::deliver_comment_added( comment )
+
+    Thread.new( comment ) do |cmt|
+      CommentNotifier::deliver_comment_added( cmt )
+    end
+  rescue
+    # ignore?, comment notification shouldn't crash the blog
   end
 end
