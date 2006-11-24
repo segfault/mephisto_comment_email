cd0eeab647e0c37ecc005a1d2f4975856942c9bc
segfault <segfault@c10dc6e2-ae21-0410-890e-c4c894b30763>
2006-11-24 21:06:23 +0000

    switching to html for the mail added links and more info
    git-svn-id: svn://hasno.info/mephisto/plugins/mephisto_comment_email@34 c10dc6e2-ae21-0410-890e-c4c894b30763

--- lib/comment_notifier.rb
+++ lib/comment_notifier.rb
@@ -6,10 +6,11 @@ class CommentNotifier < ActionMailer::Base
   def comment_added( comment )
     mail_from = @@mail_from
     mail_from = comment.site.email unless mail_from
-    from        "%s <%s>" % [ @@mail_from_prefix, mail_from ]
-    recipients  comment.article.user.email
-    subject     "%s %s posted a comment about \"%s\"" % [ @@mail_prefix, comment.author, comment.article.title ]
-    body        "comment" => comment
+    from         "%s <%s>" % [ @@mail_from_prefix, mail_from ]
+    recipients   comment.article.user.email
+    subject      "%s %s posted a comment about \"%s\"" % [ @@mail_prefix, comment.author, comment.article.title ]
+    body         "comment" => comment
+    content_type "text/html"
   end
 
   def self.template_root
