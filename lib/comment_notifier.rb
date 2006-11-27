a2899be32b4585545b630212d412af8fa481470c
segfault <segfault@c10dc6e2-ae21-0410-890e-c4c894b30763>
2006-11-27 03:42:04 +0000

    added support for sending emails only when the comment is approved
    git-svn-id: svn://hasno.info/mephisto/plugins/mephisto_comment_email@40 c10dc6e2-ae21-0410-890e-c4c894b30763

--- lib/comment_notifier.rb
+++ lib/comment_notifier.rb
@@ -1,15 +1,19 @@
 class CommentNotifier < ActionMailer::Base
   @@mail_prefix = "(blog)"
-  @@mail_from_prefix = "Mephisto Blog"
+  @@mail_from_name = "Mephisto Blog"
   @@mail_from = nil
+  @@only_approved = false
   cattr_accessor :mail_prefix
-  cattr_accessor :mail_from_prefix
+  cattr_accessor :mail_from_name
   cattr_accessor :mail_from
+  cattr_accessor :only_approved
 
   def comment_added( comment )
+    return if (only_approved && !comment.approved?)
+
     mail_from = @@mail_from
     mail_from = comment.site.email unless mail_from
-    from         "%s <%s>" % [ @@mail_from_prefix, mail_from ]
+    from         "%s <%s>" % [ @@mail_from_name, mail_from ]
     recipients   comment.article.user.email
     subject      "%s %s posted a comment about \"%s\"" % [ @@mail_prefix, comment.author, comment.article.title ]
     body         "comment" => comment
