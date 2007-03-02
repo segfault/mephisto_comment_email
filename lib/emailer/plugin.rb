fc947c08dab8eeb9d5609195c21d42e8d784a752
segfault <segfault@c10dc6e2-ae21-0410-890e-c4c894b30763>
2007-03-02 23:40:47 +0000

    updated to match trunk mephisto changes
    git-svn-id: svn://hasno.info/mephisto/plugins/mephisto_comment_email@87 c10dc6e2-ae21-0410-890e-c4c894b30763

--- lib/emailer/plugin.rb
+++ lib/emailer/plugin.rb
@@ -1,8 +1,8 @@
 # $Id$
-module MephistoPlugins
-  class CommentEmail < MephistoPlugin
+module Mephisto::Plugins
+  class CommentEmail < Mephisto::Plugin
     author 'Mark Guzman'
-    version '$Rev$'.gsub( "Rev: ", "" )
+    version 'r$Rev$'.gsub( /(\$Rev:\s+)|(\s+\$)/, "" )
     notes "Sends an email with details when a comment is posted"
     homepage "http://hasno.info/2006/11/11/mephisto-plugins"
     
